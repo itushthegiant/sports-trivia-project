fd2ab5e2efba7431d4cad6cf0f10dd0197f5993b
etamar <etamar.levy@gmail.com>
2021-05-08 14:23:55 -0400

    questions separation fixed

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ function renderQuestion(data) {
     const incorrectAnswers = data.incorrect_answers;
     const allAnswers = shuffle([...incorrectAnswers, correctAnswer]);
     console.log(allAnswers, correctAnswer)
-    div.setAttribute('class', 'card');
+    div.setAttribute('class', 'block');
     div.innerHTML = `
+        <div class='block'>
             <div class="content is-large">
                 <div class="tile is-child notification is-info">
                     <p>${question}</p>
@@ -47,7 +48,9 @@ function renderQuestion(data) {
                     </ul>
                 </div>
             </div>
-        `
+        </div>
+                    `;
+            
     questDiv.appendChild(div)
     div.addEventListener('click', (e) => checkAnswer(e, div, correctAnswer))
 }
@@ -58,8 +61,8 @@ function renderQuestion(data) {
 ////correct & incorrect answers///
 //////////////////////////////////
 function checkAnswer(e, div, correctAnswer) {
-    if (e.target.className === 'answer' && div.className !== "selected") {
-       div.className = 'selected'
+    if (e.target.className === 'answer' && div.className !== "selected block") {
+       div.className = 'selected block'
        if (e.target.innerText === correctAnswer) {
            e.target.style.color = 'green';
            const correctCounter = document.getElementById('correct-counter');
@@ -79,7 +82,7 @@ function checkAnswer(e, div, correctAnswer) {
 /////create a list of answers/////
 //////////////////////////////////
 function renderAnswers(answers) {
-    return answers.map(answer => `<li class='answer'><i class="fab fa-diaspora"></i>${answer}</li>`).join('');
+    return answers.map(answer => `<div class='block'><li class='answer'><i class="fab fa-diaspora"></i>${answer}</li></div>`).join('');
 }
 
 
@@ -99,9 +102,11 @@ function attachEventListeners() {
         numOfQuestions = e.target.value
     })
     startButton.addEventListener('click', () => {
+        const resetDiv = document.getElementById("reset-div");
         const counter = document.getElementById('counters')
         fetchQuestions()
         counter.removeAttribute('hidden');
+        resetDiv.removeAttribute('hidden');
     })
 }
 
